dacd1cab33a29d360fcdcc0ec387aa88f97348be
Yangyao Chen <yangyaochen.astro@foxmail.com>
2023-08-19 01:33:43 +0800

    update to 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'HaloFactory'
 copyright = '2023, Yangyao Chen, Kai Wang'
 author = 'Yangyao Chen, Kai Wang'
-release = '0.0.1'
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
